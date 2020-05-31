04c3b98c54f7c7c117df9a70fccc9fa934db1ae3
Owen <owen.allemang@gmail.com>
2020-05-31 19:32:54 +0200

    new commit

--- Sounding_Rocket_2DOF.m
+++ Sounding_Rocket_2DOF.m
@@ -15,7 +15,7 @@ ca = 0.
 alpha = 0.
 lrampe = 10.
 D = 0.2
-A = D^2*pi/4
+A = D^2 * pi / 4
 
 % Genereric Constants
 
@@ -29,52 +29,50 @@ g0 = 9.81
 m8 = 26.
 Is = 1700.
 tc = 15.
-mpunkt_quer = m8/tc
-Itot = m8*Is
-F_quer = Itot/tc
+mpunkt_quer = m8 / tc
+Itot = m8 * Is
+F_quer = Itot / tc
 
 % Calculated Rocket Data
 
-mc = m0-m8
-R = m0/mc
+mc = m0 - m8
+R = m0 / mc
 
 % Numerical solution
-gamma = 80*pi/180
+gamma = 80 * pi / 180
 
-[T,Y] = ode15s(@Rocket_2DOF,[0 101],[0 r0 m0 gamma 0]);
+[T,Y] = ode15s(@Rocket_2DOF, [0 101], [0 r0 m0 gamma 0]);
 
 % Calculate accelerations as derivatives from velocities
-    deltav = diff(Y(:,1));
+    deltav = diff(Y(:, 1));
     deltat = diff(T);
     lt = length(T)
     for j=1 : lt-1
         acc(j) = deltav(j) / deltat(j);
     end
-    acc(lt) = acc(lt-1)
+    acc(lt) = acc(lt - 1)
     if length (acc) > lt
         la = length(acc);
-        acc(lt+1:la) = [];
+        acc(lt + 1:la) = [];
     end
 % plot results   
 subplot(3,2,1)
-plot (T,acc,'b-')
+plot (T, acc, 'b-')
 ylabel ('Acceleration [m/s^2]')
 xlabel ('Time [s]')
-subplot(3,2,2)
-plot (T,Y(:,1),'r-')
+subplot(3, 2, 2)
+plot (T, Y(:, 1), 'r-')
 ylabel ('Velocity [m/s]')
 xlabel ('Time [s]')
-subplot(3,2,3)
-plot (T,Y(:,2)-r0,'b-')
+subplot(3, 2, 3)
+plot (T, Y(:, 2)-r0, 'b-')
 ylabel ('Altitude [m]')
 xlabel ('Time [s]')
-subplot(3,2,4)
-plot (T,Y(:,3))
+subplot(3, 2, 4)
+plot (T, Y(:, 3))
 ylabel ('Mass [kg]')
 xlabel ('Time [s]')
-subplot(3,2,5)
-plot (T,Y(:,4)*180/pi)
+subplot(3, 2, 5)
+plot (T, Y(:, 4) * 180 / pi)
 ylabel ('gamma [\gamma]')
 xlabel ('Time [s]')
-
-
